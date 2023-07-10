5656139014af035e1764faa5defe0ca03328bbaa
Matheus Iam <matheusiam27@gmail.com>
2023-07-10 16:54:40 -0400

    Entidade usuário

--- src/main/java/com/matheusiam/crudspring/entities/Usuario.java
+++ src/main/java/com/matheusiam/crudspring/entities/Usuario.java
@@ -1,9 +1,95 @@
 package com.matheusiam.crudspring.entities;
 
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "tb_usuarios")
-public class Usuario {
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Integer id;
 	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "cpf")
+	private String cpf;
+	
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
+	
+	@Column(name = "sexo", length = 1)
+	private String sexo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", dataNascimento=" + dataNascimento
+				+ ", sexo=" + sexo + "]";
+	}
 	
 }
